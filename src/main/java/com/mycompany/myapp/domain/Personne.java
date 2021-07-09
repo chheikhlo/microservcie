532bc075ae573e7cb00c7bafe46ce15da59288c4
chheikhlo <chheikhlo@gmail.com>
2021-07-09 15:15:44 +0000

    first commit

---
+++ src/main/java/com/mycompany/myapp/domain/Personne.java
@@ -0,0 +1,114 @@
+package com.mycompany.myapp.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+/**
+ * A Personne.
+ */
+@Entity
+@Table(name = "personne")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Personne implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "prenom")
+    private String prenom;
+
+    @Column(name = "tel")
+    private Integer tel;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Personne id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public Personne nom(String nom) {
+        this.nom = nom;
+        return this;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return this.prenom;
+    }
+
+    public Personne prenom(String prenom) {
+        this.prenom = prenom;
+        return this;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Integer getTel() {
+        return this.tel;
+    }
+
+    public Personne tel(Integer tel) {
+        this.tel = tel;
+        return this;
+    }
+
+    public void setTel(Integer tel) {
+        this.tel = tel;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Personne)) {
+            return false;
+        }
+        return id != null && id.equals(((Personne) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Personne{" +
+            "id=" + getId() +
+            ", nom='" + getNom() + "'" +
+            ", prenom='" + getPrenom() + "'" +
+            ", tel=" + getTel() +
+            "}";
+    }
+}
---
+++ src/test/java/com/mycompany/myapp/web/rest/PersonneResourceIT.java
@@ -0,0 +1,384 @@
+package com.mycompany.myapp.web.rest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.mycompany.myapp.IntegrationTest;
+import com.mycompany.myapp.domain.Personne;
+import com.mycompany.myapp.repository.PersonneRepository;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Integration tests for the {@link PersonneResource} REST controller.
+ */
+@IntegrationTest
+@AutoConfigureMockMvc
+@WithMockUser
+class PersonneResourceIT {
+
+    private static final String DEFAULT_NOM = "AAAAAAAAAA";
+    private static final String UPDATED_NOM = "BBBBBBBBBB";
+
+    private static final String DEFAULT_PRENOM = "AAAAAAAAAA";
+    private static final String UPDATED_PRENOM = "BBBBBBBBBB";
+
+    private static final Integer DEFAULT_TEL = 1;
+    private static final Integer UPDATED_TEL = 2;
+
+    private static final String ENTITY_API_URL = "/api/personnes";
+    private static final String ENTITY_API_URL_ID = ENTITY_API_URL + "/{id}";
+
+    private static Random random = new Random();
+    private static AtomicLong count = new AtomicLong(random.nextInt() + (2 * Integer.MAX_VALUE));
+
+    @Autowired
+    private PersonneRepository personneRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private MockMvc restPersonneMockMvc;
+
+    private Personne personne;
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Personne createEntity(EntityManager em) {
+        Personne personne = new Personne().nom(DEFAULT_NOM).prenom(DEFAULT_PRENOM).tel(DEFAULT_TEL);
+        return personne;
+    }
+
+    /**
+     * Create an updated entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Personne createUpdatedEntity(EntityManager em) {
+        Personne personne = new Personne().nom(UPDATED_NOM).prenom(UPDATED_PRENOM).tel(UPDATED_TEL);
+        return personne;
+    }
+
+    @BeforeEach
+    public void initTest() {
+        personne = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    void createPersonne() throws Exception {
+        int databaseSizeBeforeCreate = personneRepository.findAll().size();
+        // Create the Personne
+        restPersonneMockMvc
+            .perform(post(ENTITY_API_URL).contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(personne)))
+            .andExpect(status().isCreated());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeCreate + 1);
+        Personne testPersonne = personneList.get(personneList.size() - 1);
+        assertThat(testPersonne.getNom()).isEqualTo(DEFAULT_NOM);
+        assertThat(testPersonne.getPrenom()).isEqualTo(DEFAULT_PRENOM);
+        assertThat(testPersonne.getTel()).isEqualTo(DEFAULT_TEL);
+    }
+
+    @Test
+    @Transactional
+    void createPersonneWithExistingId() throws Exception {
+        // Create the Personne with an existing ID
+        personne.setId(1L);
+
+        int databaseSizeBeforeCreate = personneRepository.findAll().size();
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restPersonneMockMvc
+            .perform(post(ENTITY_API_URL).contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(personne)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    void getAllPersonnes() throws Exception {
+        // Initialize the database
+        personneRepository.saveAndFlush(personne);
+
+        // Get all the personneList
+        restPersonneMockMvc
+            .perform(get(ENTITY_API_URL + "?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(personne.getId().intValue())))
+            .andExpect(jsonPath("$.[*].nom").value(hasItem(DEFAULT_NOM)))
+            .andExpect(jsonPath("$.[*].prenom").value(hasItem(DEFAULT_PRENOM)))
+            .andExpect(jsonPath("$.[*].tel").value(hasItem(DEFAULT_TEL)));
+    }
+
+    @Test
+    @Transactional
+    void getPersonne() throws Exception {
+        // Initialize the database
+        personneRepository.saveAndFlush(personne);
+
+        // Get the personne
+        restPersonneMockMvc
+            .perform(get(ENTITY_API_URL_ID, personne.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.id").value(personne.getId().intValue()))
+            .andExpect(jsonPath("$.nom").value(DEFAULT_NOM))
+            .andExpect(jsonPath("$.prenom").value(DEFAULT_PRENOM))
+            .andExpect(jsonPath("$.tel").value(DEFAULT_TEL));
+    }
+
+    @Test
+    @Transactional
+    void getNonExistingPersonne() throws Exception {
+        // Get the personne
+        restPersonneMockMvc.perform(get(ENTITY_API_URL_ID, Long.MAX_VALUE)).andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    void putNewPersonne() throws Exception {
+        // Initialize the database
+        personneRepository.saveAndFlush(personne);
+
+        int databaseSizeBeforeUpdate = personneRepository.findAll().size();
+
+        // Update the personne
+        Personne updatedPersonne = personneRepository.findById(personne.getId()).get();
+        // Disconnect from session so that the updates on updatedPersonne are not directly saved in db
+        em.detach(updatedPersonne);
+        updatedPersonne.nom(UPDATED_NOM).prenom(UPDATED_PRENOM).tel(UPDATED_TEL);
+
+        restPersonneMockMvc
+            .perform(
+                put(ENTITY_API_URL_ID, updatedPersonne.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(TestUtil.convertObjectToJsonBytes(updatedPersonne))
+            )
+            .andExpect(status().isOk());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeUpdate);
+        Personne testPersonne = personneList.get(personneList.size() - 1);
+        assertThat(testPersonne.getNom()).isEqualTo(UPDATED_NOM);
+        assertThat(testPersonne.getPrenom()).isEqualTo(UPDATED_PRENOM);
+        assertThat(testPersonne.getTel()).isEqualTo(UPDATED_TEL);
+    }
+
+    @Test
+    @Transactional
+    void putNonExistingPersonne() throws Exception {
+        int databaseSizeBeforeUpdate = personneRepository.findAll().size();
+        personne.setId(count.incrementAndGet());
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restPersonneMockMvc
+            .perform(
+                put(ENTITY_API_URL_ID, personne.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(TestUtil.convertObjectToJsonBytes(personne))
+            )
+            .andExpect(status().isBadRequest());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    void putWithIdMismatchPersonne() throws Exception {
+        int databaseSizeBeforeUpdate = personneRepository.findAll().size();
+        personne.setId(count.incrementAndGet());
+
+        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
+        restPersonneMockMvc
+            .perform(
+                put(ENTITY_API_URL_ID, count.incrementAndGet())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(TestUtil.convertObjectToJsonBytes(personne))
+            )
+            .andExpect(status().isBadRequest());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    void putWithMissingIdPathParamPersonne() throws Exception {
+        int databaseSizeBeforeUpdate = personneRepository.findAll().size();
+        personne.setId(count.incrementAndGet());
+
+        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
+        restPersonneMockMvc
+            .perform(put(ENTITY_API_URL).contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(personne)))
+            .andExpect(status().isMethodNotAllowed());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    void partialUpdatePersonneWithPatch() throws Exception {
+        // Initialize the database
+        personneRepository.saveAndFlush(personne);
+
+        int databaseSizeBeforeUpdate = personneRepository.findAll().size();
+
+        // Update the personne using partial update
+        Personne partialUpdatedPersonne = new Personne();
+        partialUpdatedPersonne.setId(personne.getId());
+
+        partialUpdatedPersonne.nom(UPDATED_NOM);
+
+        restPersonneMockMvc
+            .perform(
+                patch(ENTITY_API_URL_ID, partialUpdatedPersonne.getId())
+                    .contentType("application/merge-patch+json")
+                    .content(TestUtil.convertObjectToJsonBytes(partialUpdatedPersonne))
+            )
+            .andExpect(status().isOk());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeUpdate);
+        Personne testPersonne = personneList.get(personneList.size() - 1);
+        assertThat(testPersonne.getNom()).isEqualTo(UPDATED_NOM);
+        assertThat(testPersonne.getPrenom()).isEqualTo(DEFAULT_PRENOM);
+        assertThat(testPersonne.getTel()).isEqualTo(DEFAULT_TEL);
+    }
+
+    @Test
+    @Transactional
+    void fullUpdatePersonneWithPatch() throws Exception {
+        // Initialize the database
+        personneRepository.saveAndFlush(personne);
+
+        int databaseSizeBeforeUpdate = personneRepository.findAll().size();
+
+        // Update the personne using partial update
+        Personne partialUpdatedPersonne = new Personne();
+        partialUpdatedPersonne.setId(personne.getId());
+
+        partialUpdatedPersonne.nom(UPDATED_NOM).prenom(UPDATED_PRENOM).tel(UPDATED_TEL);
+
+        restPersonneMockMvc
+            .perform(
+                patch(ENTITY_API_URL_ID, partialUpdatedPersonne.getId())
+                    .contentType("application/merge-patch+json")
+                    .content(TestUtil.convertObjectToJsonBytes(partialUpdatedPersonne))
+            )
+            .andExpect(status().isOk());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeUpdate);
+        Personne testPersonne = personneList.get(personneList.size() - 1);
+        assertThat(testPersonne.getNom()).isEqualTo(UPDATED_NOM);
+        assertThat(testPersonne.getPrenom()).isEqualTo(UPDATED_PRENOM);
+        assertThat(testPersonne.getTel()).isEqualTo(UPDATED_TEL);
+    }
+
+    @Test
+    @Transactional
+    void patchNonExistingPersonne() throws Exception {
+        int databaseSizeBeforeUpdate = personneRepository.findAll().size();
+        personne.setId(count.incrementAndGet());
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restPersonneMockMvc
+            .perform(
+                patch(ENTITY_API_URL_ID, personne.getId())
+                    .contentType("application/merge-patch+json")
+                    .content(TestUtil.convertObjectToJsonBytes(personne))
+            )
+            .andExpect(status().isBadRequest());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    void patchWithIdMismatchPersonne() throws Exception {
+        int databaseSizeBeforeUpdate = personneRepository.findAll().size();
+        personne.setId(count.incrementAndGet());
+
+        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
+        restPersonneMockMvc
+            .perform(
+                patch(ENTITY_API_URL_ID, count.incrementAndGet())
+                    .contentType("application/merge-patch+json")
+                    .content(TestUtil.convertObjectToJsonBytes(personne))
+            )
+            .andExpect(status().isBadRequest());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    void patchWithMissingIdPathParamPersonne() throws Exception {
+        int databaseSizeBeforeUpdate = personneRepository.findAll().size();
+        personne.setId(count.incrementAndGet());
+
+        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
+        restPersonneMockMvc
+            .perform(patch(ENTITY_API_URL).contentType("application/merge-patch+json").content(TestUtil.convertObjectToJsonBytes(personne)))
+            .andExpect(status().isMethodNotAllowed());
+
+        // Validate the Personne in the database
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    void deletePersonne() throws Exception {
+        // Initialize the database
+        personneRepository.saveAndFlush(personne);
+
+        int databaseSizeBeforeDelete = personneRepository.findAll().size();
+
+        // Delete the personne
+        restPersonneMockMvc
+            .perform(delete(ENTITY_API_URL_ID, personne.getId()).accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+
+        // Validate the database contains one less item
+        List<Personne> personneList = personneRepository.findAll();
+        assertThat(personneList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+}
